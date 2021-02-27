6c013baeeed612155b2f2a0dd45df83bd15f8575
JakeMagro28 <jake.magro.e21476@mcast.edu.mt>
2021-02-27 02:41:22 +0100

    Update 1

---
+++ Synoptic/Assets/Scripts/Killbox.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Killbox : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D football)
+    {
+        print("Collision with " + football.name);
+        Destroy(football.gameObject);
+
+    }
+
+
+    
+
+
+}
---
+++ Synoptic/Assets/Scripts/Player.cs
@@ -0,0 +1,130 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Threading;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+
+
+    [SerializeField] float moveSpeed = 10f;
+
+    [SerializeField] float xpadding = 0.4f;
+
+    [SerializeField] float ypadding = 1.8f;
+
+    [SerializeField] GameObject footballPrefab;
+
+    [SerializeField] float kickTime = 2f;
+
+
+
+    float xMin, xMax, yMin, yMax;
+
+
+    Coroutine kickCoroutine;
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        SetUpMoveBoundaries();
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        Move();
+        Kick();
+    }
+
+    private IEnumerator kickContinuously()
+    {
+        while (true)
+        {
+            //kick a ball
+            GameObject football = Instantiate(footballPrefab, transform.position, Quaternion.identity);
+            // Ask teacher tmrw 
+            //gives velocity to each ball
+            football.GetComponent<Rigidbody2D>().velocity = new Vector2(0f, 15f);
+
+            yield return new WaitForSeconds(kickTime);
+
+
+
+        }
+
+    }
+
+
+
+
+    private void Kick()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+
+            kickCoroutine = StartCoroutine(kickContinuously());
+        }
+
+        if(Input.GetButtonUp("Fire1"))
+        {
+
+            StopCoroutine(kickCoroutine);
+        }
+
+
+    }
+
+
+    
+
+
+
+
+    //boundaries
+    private void SetUpMoveBoundaries()
+    {
+        Camera gameCamera = Camera.main;
+
+        xMin = gameCamera.ViewportToWorldPoint(new Vector3(0, 0, 0)).x + xpadding;
+
+        xMax = gameCamera.ViewportToWorldPoint(new Vector3(1, 0, 0)).x - xpadding;
+
+        yMin = gameCamera.ViewportToWorldPoint(new Vector3(0, 0, 0)).y + ypadding;
+
+        yMax = gameCamera.ViewportToWorldPoint(new Vector3(0, (float)0.6, 0)).y - ypadding;
+
+    }
+    
+
+
+    // Moves the player
+    private void Move()
+    {
+        // deltax changes on player movment
+        var deltaX = Input.GetAxis("Horizontal") * Time.deltaTime * moveSpeed;
+
+        //current x + deltaX
+        var newXPos = transform.position.x + deltaX;
+        newXPos = Mathf.Clamp(newXPos, xMin, xMax);
+
+        // deltax changes on player movment
+        var deltaY = Input.GetAxis("Vertical") * Time.deltaTime * moveSpeed;
+
+        //current y + deltaY
+        var newYPos = transform.position.y + deltaY;
+        newYPos = Mathf.Clamp(newYPos, yMin, yMax);
+
+        //move the player to the new x position
+        this.transform.position = new Vector2(newXPos, newYPos);
+
+
+    }
+
+
+
+
+}
